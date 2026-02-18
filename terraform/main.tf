558e5121f59cd9c529cb3bf715850d823f0358d7
Lesly031104 <leslymedher031104@gmail.com>
2026-02-18 00:01:16 -0600

    Repositorio limpio sin archivos pesados

---
+++ terraform/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_amplify_app" "hola_mundo" {
+  name        = "hola-mundo-vite"
+  repository  = var.repo_url
+  oauth_token = var.github_token
+
+  build_spec = <<-EOT
+version: 1
+frontend:
+  phases:
+    preBuild:
+      commands:
+        - npm ci
+    build:
+      commands:
+        - npm run build
+  artifacts:
+    baseDirectory: dist
+    files:
+      - '**/*'
+  cache:
+    paths:
+      - node_modules/**/*
+EOT
+
+  custom_rule {
+    source = "/<*>"
+    target = "/index.html"
+    status = "200"
+  }
+}
+
+resource "aws_amplify_branch" "main" {
+  app_id      = aws_amplify_app.hola_mundo.id
+  branch_name = "main"
+}
