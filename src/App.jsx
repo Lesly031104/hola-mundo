c7c3282b39097d0026090dbd9b32290130bbf21e
Lesly031104 <leslymedher031104@gmail.com>
2026-02-19 12:23:07 -0600

    Fix App.jsx for Amplify build

--- src/App.jsx
+++ src/App.jsx
@@ -9,22 +9,26 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hola Mundo desde AWS 🚀</h1>
+
+      <h1>Holaaa Mundo desde AWS 🚀</h1>
+
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((c) => c + 1)}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
